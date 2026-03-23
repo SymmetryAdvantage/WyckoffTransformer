5bcd98b5477a828ec6cacfd375e9c324fb92688e
Nikita Kazeev <kazeevn@gmail.com>
2026-03-24 02:33:42 +0800

    Deps

--- scripts/zeus_env_init.sh
+++ scripts/zeus_env_init.sh
@@ -3,4 +3,5 @@
 poetry env use python
 poetry run pip install --upgrade pip
 poetry run pip install /mnt/hdd/torch_wheels/torch-2.10.0-cp312-cp312-linux_x86_64.whl /mnt/hdd/torch_wheels/torch_scatter-2.1.2-cp312-cp312-linux_x86_64.whl /mnt/hdd/torch_wheels/torch_sparse-0.6.18-cp312-cp312-linux_x86_64.whl /mnt/hdd/torch_wheels/pyg_lib-0.5.0-cp312-cp312-linux_x86_64.whl
+poetry run pip install triton==3.6.0
 poetry install
