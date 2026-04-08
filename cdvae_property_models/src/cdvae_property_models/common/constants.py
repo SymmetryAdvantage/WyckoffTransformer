94ae8a84e64c43090bd36c07084fae7d5d1d28a6
Nikita Kazeev <kazeevn@gmail.com>
2026-04-08 12:26:55 +0800

    Bump version to 1.0.2 and restore matminer as a dependency

--- cdvae_property_models/src/cdvae_property_models/common/constants.py
+++ cdvae_property_models/src/cdvae_property_models/common/constants.py
@@ -1,3 +1,5 @@
+# Original: https://github.com/txie-93/cdvae/tree/main
+# Modifications copyright 2026, Nikita Kazeev. Licensed under the Apache License, Version 2.0.
 CompScalerMeans = [
     21.194441759304013,
     58.20212663122281,
