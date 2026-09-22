64d8ba4b92c6c92c5411687af8e0f1a9776bf816
Nikita Kazeev <kazeevn@gmail.com>
2026-09-22 23:19:46 +0800

    iapetus scrpt

--- scripts/platforms/iapetus/protocol.sh
+++ scripts/platforms/iapetus/protocol.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-alias run=scripts/platforms/iapetus/run.sh
-run wyformer-protocol-wandb $1 --output-dir generated/$1/protocol --pyxtal-cores 6 --devices cuda:0,cuda:0,cuda:1,cuda:1,cuda:2
+scripts/platforms/iapetus/run.sh wyformer-protocol-wandb $1 --output-dir generated/$1/protocol --pyxtal-cores 6 --devices cuda:0,cuda:0,cuda:1,cuda:1,cuda:2 --skip-generate
